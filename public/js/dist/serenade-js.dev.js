fe0da0ab3945434960938021fdee54167cd3a287
eileencchen <hantzu001@gmail.com>
2022-07-04 17:50:31 +0800

    0.3.7 (2022.07.04)

---
+++ public/js/dist/serenade-js.dev.js
@@ -0,0 +1,28 @@
+"use strict";
+
+//================
+// 手機版 PC版 menu nav
+//================
+// 定義視窗寬度的變數
+var windowWidth = $(window).width(); // 進網頁直接執行一次 判斷
+
+if (windowWidth >= 768) {
+  // 大於等於768執行
+  // $('nav>ul>li').hover(function(){}, function(){})
+  $('.main-nav').hover(function () {
+    // 滑鼠滑入執行
+    $(this).find('.submenu').addClass('active');
+  }, function () {
+    // 滑鼠滑出執行
+    $(this).find('.submenu').removeClass('active');
+  });
+} else {
+  // 小於768執行
+  $('#btn-nav-switch').on('click', function () {
+    $('.main-nav').toggleClass('active');
+  }); // btn-nav-switch end
+
+  $('nav>ul>li').on('click', function () {
+    $(this).find('.submenu').toggleClass('active');
+  }); // nav>ul>li end
+} // windowWidth>=768 end
